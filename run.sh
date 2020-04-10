55b52ee96524d4b109021e561eae91b1c4fa47b3
Peyton Casper <peytoncas@gmail.com>
2020-04-10 14:08:20 -0400

    Updated script

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
-export VAULT_ADDR=${VAULT_ADDR:-http://vault:8200}
+#!/bin/bash
+#export VAULT_ADDR=${VAULT_ADDR:-http://vault:8200}
 
 # Get our token
 export SA_TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
@@ -17,22 +18,4 @@ export VAULT_TOKEN=${VAULT_TOKEN:-INVALID}
 # Fetch a KV
 export KV=$(curl -H "X-Vault-Token: $VAULT_TOKEN" $VAULT_ADDR/v1/openshift/test)
 
-# Generate sample HTML page:
-cat > /usr/share/nginx/html/index.html <<EOF
-<html><head><title>Vault Secrets POD</title></head><body><span style="font-family: monospace;">
-<h2>Welcome</h2><p>
-<table width="1680" border="1">
-<tr><td nowrap><b>My service account token</b>:</td><td>$SA_TOKEN</td></tr>
-<tr><td nowrap><b>My Vault token</b>:</td><td>$VAULT_TOKEN</td></tr>
-</table>
-The secrets I've fetched during startup:
-<table width="1680" border="1">
-<tr><td nowrap><b>Static secret kv/test</b>:</td><td>$KV</td></tr>
-</table>
-End
-</body></html>
-EOF
-
-sed -i 's/$/<br>/g' /usr/share/nginx/html/index.html
-
-nginx -g "daemon off;"
+echo $KV
